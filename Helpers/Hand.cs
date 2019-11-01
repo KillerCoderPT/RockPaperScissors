97936c962b902a93f66cb516bea4816677b21fde
nugpt <nuno_garcia@icloud.com>
2019-11-01 19:40:21 +0000

    Initial Commit. The logic is done, just need to add Authentication

---
+++ Helpers/Hand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors.Helpers
+{
+    public class Hand
+    {
+        public OptionRPS Selection { get; set; }
+        public OptionRPS WinsAgaints { get; set; }
+        public OptionRPS LosesAgaints { get; set; }
+        public string Image { get; set; }
+
+        public GameStatus PlayAgainst(Hand opponentHand)
+        {
+            if (Selection == opponentHand.Selection)
+            {
+                return GameStatus.Draw;
+            }
+            else if (LosesAgaints == opponentHand.Selection)
+            {
+                return GameStatus.Loss;
+            }
+
+            return GameStatus.Victory;
+        }
+    }
+}
---
+++ Helpers/OptionRPS.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors.Helpers
+{
+    public enum OptionRPS
+    {
+        Rock, Paper, Scissors
+    }
+}
